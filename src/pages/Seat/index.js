8deebf6ef5a38f681f2635259fface95e7c352c8
gsnorberto <sn_gabriel@outlook.com>
2022-12-07 12:03:40 -0300

    style: Layout da página Assentos

--- src/pages/Seat/index.js
+++ src/pages/Seat/index.js
@@ -1,5 +1,57 @@
+import { Button, ButtonArea, Inputs, SeatArea, Seats, St, Status, Title, Label, Input, Footer } from "./styles"
+
 export default () => {
-    return(
-        <div>Seat</div>
+    return (
+        <SeatArea>
+            <Title>Selecione o(s) assento(s)</Title>
+
+            <Seats>
+                <St>1</St>
+                <St>1</St>
+                <St>1</St>
+                <St>1</St>
+                <St>1</St>
+                <St>1</St>
+                <St>1</St>
+                <St>1</St>
+                <St>1</St>
+            </Seats>
+
+            <Status>
+                <div className="status">
+                    <div className="circle green"></div>
+                    <div className="title">Selecionado</div>
+                </div>
+                <div className="status">
+                    <div className="circle gray"></div>
+                    <div className="title">Disponível</div>
+                </div>
+                <div className="status">
+                    <div className="circle yellow"></div>
+                    <div className="title">Indisponível</div>
+                </div>
+            </Status>
+
+            <Inputs>
+                <Label>
+                    Nome do comprador:
+                    <Input placeholder="Digite seu nome..." />
+                </Label>
+
+                <Label>
+                    CPF do comprador:
+                    <Input placeholder="Digite seu CPF..." />
+                </Label>
+            </Inputs>
+
+            <ButtonArea>
+                <Button>Reservar Assento</Button>
+            </ButtonArea>
+
+            <Footer>
+                <img src="" alt="" />
+                <div className="title">Teste...</div>
+            </Footer>
+        </SeatArea>
     )
 }
--- src/pages/Seat/styles.js
+++ src/pages/Seat/styles.js
@@ -1,1 +1,143 @@
 import styled from "styled-components";
+
+export const SeatArea = styled.div`
+    font-family: 'Roboto', sans-serif;
+    max-width: 1170px;
+    margin: 0 auto;
+    padding-top: 70px;
+    padding-bottom: 120px;
+`
+
+export const Title = styled.div`
+    text-align: center;
+    margin: 40px 20px;
+    color: #293845;
+    font-size: 24px;
+`
+
+export const Seats = styled.div`
+    margin: 0 20px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+`
+
+export const St = styled.div`
+    width: 26px;
+    height: 26px;
+    background-color: #C3CFD9;
+    font-size: 11px;
+    border-radius: 50%;
+    border: 1px solid #808F9D;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 5px;
+`
+
+export const Status = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+    flex-wrap: wrap;
+
+    .status{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
+        margin: 0 10px;
+    }
+    .circle{
+        width: 26px;
+        height: 26px;
+        border-radius: 50%;
+        margin-bottom: 10px;
+    }
+    .green{
+        background-color: #1AAE9E;
+        border: 1px solid #0E7D71;
+    }
+    .gray{
+        background-color: #C3CFD9;
+        border: 1px solid #7B8B99;
+    }
+    .yellow{
+        background-color: #FBE192;
+        border: 1px solid #F7C52B;
+    }
+    .title{
+        font-size: 13px;
+        color: #4E5A65;
+    }
+`
+
+export const Inputs = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+    
+`
+export const Label = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-size: 18px;
+    color: #293845;
+    margin-bottom: 15px;
+    
+`
+export const Input = styled.input`
+    margin-top: 5px;
+    border: 1px solid #D4D4D4;
+    max-width: 330px;
+    height: 30px;
+    outline: 0;
+    padding: 5px 15px;
+    font-size: 18px;
+    font-style: italic;
+    color: #293845;
+`
+
+export const Button = styled.button`
+    border: 0;
+    background-color: #E8833A;
+    padding: 15px 30px;
+    color: #FFFFFF;
+    font-size: 18px;
+    border-radius: 3px;
+    cursor: pointer;
+
+    &:hover{
+        opacity: 0.8;
+    }
+`
+export const ButtonArea = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+`
+
+export const Footer = styled.div`
+   width: 100%;
+   height: 115px;
+   background-color: #DFE6ED;
+   display: flex;
+   align-items: center;
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   padding: 0 20px;
+
+   img{
+    width: 64px;
+    border: 5px solid #FFFFFF;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    margin-right: 15px;
+   }
+
+   .title{
+    font-size: 26px;
+    color: #293845;
+   }
+`
