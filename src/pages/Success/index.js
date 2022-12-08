991d51423d76990aa045af52cf0adbf86054159f
gsnorberto <sn_gabriel@outlook.com>
2022-12-08 08:22:45 -0300

    style: Layout da página de sucesso

--- src/pages/Success/index.js
+++ src/pages/Success/index.js
@@ -1,5 +1,25 @@
+import { SuccessArea, Title, Title2, Desc, Movie, Tickets, Buyer } from "./styles"
+
 export default () => {
     return(
-        <div>Success</div>
+        <SuccessArea>
+            <Title>Pedido feito com sucesso!</Title>
+
+            <Movie>
+                <Title2>Filme e Sessão</Title2>
+                <Desc>Teste...</Desc>
+                <Desc>Teste...</Desc>
+            </Movie>
+            <Tickets>
+                <Title2>Ingressos</Title2>
+                <Desc>Numéro 15</Desc>
+                <Desc>Numéro 16</Desc>
+            </Tickets>
+            <Buyer>
+                <Title2>Comprador</Title2>
+                <Desc>Ciclano de Souza</Desc>
+                <Desc>111.222.333-55</Desc>
+            </Buyer>
+        </SuccessArea>
     )
 }
--- src/pages/Success/styles.js
+++ src/pages/Success/styles.js
@@ -1,1 +1,44 @@
 import styled from "styled-components";
+
+export const SuccessArea = styled.div`
+    font-family: 'Roboto', sans-serif;
+    max-width: 1170px;
+    margin: 0 auto;
+    padding: 100px 20px 120px 20px;
+`
+export const Title = styled.div`
+    text-align: center;
+    font-size: 24px;
+    font-weight: bold;
+    color: #247A6B;
+    margin-bottom: 40px;
+`
+export const Movie = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+`
+
+export const Tickets = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+`
+
+export const Buyer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const Title2 = styled.div`
+    font-size: 24px;
+    font-weight: bold;
+    color: #293845;
+`
+
+export const Desc = styled.div`
+    font-size: 22px;
+    color: #293845;
+    margin-top: 5px;
+`
+
